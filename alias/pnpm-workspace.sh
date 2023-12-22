f746b342bc880bda146f675dada2e5e8753311c9
alifprihantoro <alifprihantoro@gmail.com>
2023-12-22 19:03:26 +0700

    fix: workspace dir list fzf tobe short

--- alias/pnpm-workspace.sh
+++ alias/pnpm-workspace.sh
@@ -2,8 +2,10 @@ cfw() {
   local ARG_FIND=pnpm-workspace.yaml
   local ROOT_DIR=$(findUp $ARG_FIND)
   local LIST_YML=$(echo $(yq "((.packages[]) |= \"$ROOT_DIR/\" + . +\"/\") | .packages[]" $ROOT_DIR/$ARG_FIND) | tr '\n' ' ')
-  local DIR=$(eval ls -d $LIST_YML | fzf)
-  if [ ! -z "$DIR" ]; then
+  local LIST_DIR=$(eval ls -d $LIST_YML)
+  local DIR_FZF=$(echo ${LIST_DIR//$ROOT_DIR/} | tr ' ' '\n' | fzf)
+  if [ ! -z "$DIR_FZF" ]; then
+    local DIR="$ROOT_DIR$DIR_FZF"
     cd $DIR
   fi
 }
