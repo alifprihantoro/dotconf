7a90bef18e6b0601dfc25e857ef4b3589aa35e57
alifprihantoro <alifprihantoro@gmail.com>
2024-06-16 02:55:23 +0700

    add theme for fzf

--- alias/fzf/init.sh
+++ alias/fzf/init.sh
@@ -1,9 +1,31 @@
 # func fuzzy
-FILEFUZZY(){
+FILEFUZZY() {
   vim_w_fzf=$(FZF --preview)
-  while [ "$vim_w_fzf" != "" ]
-  do
+  while [ "$vim_w_fzf" != "" ]; do
     $1 $vim_w_fzf
     break
   done
 }
+
+# theme
+export FZF_DEFAULT_OPTS="--highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none
+  --color=bg+:#2d3f76 \
+  --color=border:#589ed7 \
+  --color=fg:#c8d3f5 \
+  --color=gutter:#1e2030 \
+  --color=header:#ff966c \
+  --color=hl+:#65bcff \
+  --color=hl:#65bcff \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#65bcff \
+  --color=query:#c8d3f5:regular \
+  --color=scrollbar:#589ed7 \
+  --color=separator:#ff966c \
+  --color=spinner:#ff007c \
+"
