d7f9feaf73cd9a056c4bd37f2dc636a5e299d7ae
alifprihantoro <alifprihantoro@gmail.com>
2024-06-20 12:31:48 +0700

    fix tmux

--- alias/tmux.sh
+++ alias/tmux.sh
@@ -1,43 +1,45 @@
 alias tmuxa='tmux attach'
 alias tmuxk='tmux kill-server'
+export TMUX_PWD=$HOME
+# args 1 => name window
+# args 2 => cmd execute
 addNewTmux() {
-  echo "new-window -n \"$1\" -c \"#{pane_current_path}\" \\;"
+  echo "neww -n \"$1\" -c \"$TMUX_PWD\" \\;"
   if [ "$2" != "" ]; then
     echo "send-keys $2 C-m \\; "
   fi
 }
 
 LIST_TMUX=('BLANK => tmux rename-session "home"')
-LIST_TMUX+=('TMUX QUEST => tmuxq')
-tmuxq() {
-  local DEFAULT_PATH="$dw/quest"
+LIST_TMUX+=('TMUX QUEST => tmuxquest')
+tmuxquest() {
+  TMUX_PWD="$dw/quest"
   local COMPILE="pnpm dev"
   local NAME_SESSION='quest'
   local NAME_FIRST_WINDOW='editor'
-  local FIRST_CMD="cd $DEFAULT_PATH && v"
+  local FIRST_CMD="cd $TMUX_PWD && v"
   # first cmd/window
-  local ARGS="rename-session $NAME_SESSION \; "
-  ARGS+="rename-window $NAME_FIRST_WINDOW  \; "
-  ARGS+="send-keys T_CMD C-m  \; "
+  local ARGS="rename-session \"$NAME_SESSION\" \; "
+  ARGS+="rename-window \"$NAME_FIRST_WINDOW\"  \; "
+  ARGS+="send-keys \"$FIRST_CMD\" C-m  \; "
   # second cmd
   ARGS+=$(addNewTmux "issue" "v")
-  ARGS+=$(addNewTmux "compile" $COMPILE)
-  ARGS+=$(addNewTmux "proot" "u C-m sta")
+  ARGS+=$(addNewTmux "compile" \"$COMPILE\")
+  ARGS+=$(addNewTmux "proot" "u C-m sta C-m")
   ARGS="tmux $(echo $ARGS | tr '\n' ' ')"
   eval $ARGS
 }
 
-LIST_TMUX+=('TMUX WEB => tmuxq')
-tmuxw() {
-  local DEFAULT_PATH="$dw/alifprihantoro"
-  local COMPILE="pnpm dev:home"
+LIST_TMUX+=('TMUX WEB => tmuxweb')
+tmuxweb() {
+  TMUX_PWD="$dw/alifprihantoro"
+  local COMPILE="pnpm\ dev:home"
   local NAME_SESSION='web'
   local NAME_FIRST_WINDOW='editor'
-  local FIRST_CMD="cd $DEFAULT_PATH && v"
   # first cmd/window
-  local ARGS="rename-session $NAME_SESSION \; "
-  ARGS+="rename-window $NAME_FIRST_WINDOW  \; "
-  ARGS+="send-keys T_CMD C-m  \; "
+  local ARGS="rename-session \"$NAME_SESSION\" \; "
+  ARGS+="rename-window \"$NAME_FIRST_WINDOW\"  \; "
+  ARGS+="send-keys \"cd $TMUX_PWD && v\" C-m \; "
   # second cmd
   ARGS+=$(addNewTmux "issue" "v")
   ARGS+=$(addNewTmux "compile" $COMPILE)
