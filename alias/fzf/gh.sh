06c2a0d0b9b92ea277f518a8b8d14a5621bdbe04
alifprihantoro <alifprihantoro@gmail.com>
2024-06-29 19:03:27 +0700

    add opts depth 1

--- alias/fzf/gh.sh
+++ alias/fzf/gh.sh
@@ -1,4 +1,12 @@
 LIST_CMD+=('fzf list repo => ghc <username>')
-ghc(){
-  git clone https://github.com/$(gh repo list $1 | fzf | awk '{print $1}')
+ghc() {
+  local getRepo=$(gh repo list $1 | fzf | awk '{print $1}')
+  if [ ! -z "$getRepo" ]; then
+    read -p "is use depth 1? (Y/N): " answer
+    local depth=''
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+      depth="depth=1"
+    fi
+    git clone https://github.com/$getRepo $depth
+  fi
 }
